da2f284603de7706d5abe2b57327ccda99ac6255
vinicius picheidt marques <viniciuspicheidt@gmail.com>
2024-05-10 20:08:03 -0300

    feat: refactoring dashboard and add cache

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,36 +2,8 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @active_people_pie_chart = Dashboard.new(user: current_user).count_active_people()
 
-    # Total somando todos associados ativos
-    active_people_ids = Person.where(active: true).select(:id)
-    @total_debts = Debt.where(person_id: active_people_ids).sum(:amount)
-    @total_payments = Payment.where(person_id: active_people_ids).sum(:amount)
-    @balance = @total_payments - @total_debts
-
-    # últimos lançamentos
-    # no formato somente id + amount para o kickchart
-    @last_debts = Debt.order(created_at: :desc).limit(10).map do |debt|
-      [debt.id, debt.amount]
-    end
-    @last_payments = Payment.order(created_at: :desc).limit(10).map do |payment|
-      [payment.id, payment.amount]
-    end
-
-    # últimos associados cadastrados pelo usuário atual
-    @my_people = Person.where(user: current_user).order(:created_at).limit(10)
-
-    people = Person.all.select do |person|
-      person.balance > 0
-    end.sort_by do |person|
-      person.balance
-    end
-
-    # associado com maior saldo
-    @top_person = people.last
-
-    # associado com menor saldo
-    @bottom_person = people.first
+    dashboard = Dashboard.new()
+    @dashboardData = dashboard.construct_dashboard(user: current_user)
   end
 end
--- app/services/dashboard.rb
+++ app/services/dashboard.rb
@@ -1,10 +1,66 @@
 class Dashboard
-    def initialize(user: current_user)
-        @user = user
+
+    def construct_dashboard(user: current_user)
+        dash = {
+            'active_people_pie_chart' => count_active_people(),
+            'total_debts' => active_people_total_debts(),
+            'total_payments' => active_people_total_payments(),
+            'last_debts' => last_debts(),
+            'last_payments' => last_payments(),
+            'my_people' => my_people(user: user),
+        }
+
+        top_bottom = top_bottom_balance_people()
+        dash['balance'] = dash['total_payments'] - dash['total_debts']
+        dash['top_person'] = top_bottom[0]
+        dash['bottom_person'] = top_bottom[1]
+
+        return dash
     end
 
+    private 
+
     def count_active_people
-        count_per_group = Person.group(:active).count
-        return { active: count_per_group[true].to_i, inactive: count_per_group[false].to_i }
+        Rails.cache.fetch("active_people", expires_in: 10.minutes) do 
+            count_per_group = Person.group(:active).count
+            { active: count_per_group[true].to_i, inactive: count_per_group[false].to_i }
+        end
+    end
+
+    def active_people_total_payments
+        Rails.cache.fetch("active_people_payment", expires_in: 10.minutes) do
+            Payment.joins(:person).where(people: { active: true }).sum(:amount)
+        end
+    end
+
+    def active_people_total_debts
+        Rails.cache.fetch("active_people_debt", expires_in: 10.minutes) do
+            Debt.joins(:person).where(people: { active: true }).sum(:amount)
+        end 
+    end
+
+    def last_debts
+        Rails.cache.fetch("last_debts", expires_in: 10.minutes) do
+            Debt.order(created_at: :desc).limit(10).pluck(:id, :amount)
+        end
+    end
+
+    def last_payments
+        Rails.cache.fetch("last_payment", expires_in: 10.minutes) do
+            Payment.order(created_at: :desc).limit(10).pluck(:id, :amount)
+        end
+    end
+
+    def my_people(user: current_user)
+        Rails.cache.fetch("#{user.id}/my_people", expires_in: 10.minutes) do
+            Person.where(user: user).order(:created_at).limit(10)
+        end
+    end
+
+    def top_bottom_balance_people
+        Rails.cache.fetch("top_bottom_balance", expires_in: 10.minutes) do
+            people = Person.all.order(balance: :desc)
+            [people.first, people.last]
+        end
     end
 end
