752bb5ce7a1d0bf04e98fc775d8c80b36befbb9a
vinicius picheidt marques <viniciuspicheidt@gmail.com>
2024-05-13 19:35:40 -0300

    feat: change lib for get email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   # mail config
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
+
   config.action_mailer.perform_deliveries = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
     end
   end
 
+
+  
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
